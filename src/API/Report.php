fdcac2df74b6aeae450c2022bd5f80aa1a3faab6
Stephan Vierkant <stephan@vierkant.net>
2021-07-09 15:56:07 +0200

    report rebind

--- src/API/Report.php
+++ src/API/Report.php
@@ -3,6 +3,7 @@
 namespace Tngnt\PBI\API;
 
 use Tngnt\PBI\Client;
+use function sprintf;
 
 /**
  * Class Report
@@ -13,6 +14,7 @@ class Report
 {
     const REPORT_URL = "https://api.powerbi.com/v1.0/myorg/reports";
     const GROUP_REPORT_URL = "https://api.powerbi.com/v1.0/myorg/groups/%s/reports";
+    const GROUP_REPORT_REBIND_URL = "https://api.powerbi.com/v1.0/myorg/groups/%s/reports/%s/";
     const GROUP_REPORT_EMBED_URL = "https://api.powerbi.com/v1.0/myorg/groups/%s/reports/%s/GenerateToken";
 
     /**
@@ -63,6 +65,28 @@ class Report
 
         $body = [
             'accessLevel' => $accessLevel,
+        ];
+
+        $response = $this->client->request(Client::METHOD_POST, $url, $body);
+
+        return $this->client->generateResponse($response);
+    }
+
+    /**
+     * Rebinds the specified report from the specified workspace to the requested dataset.
+     *
+     * @param string      $groupId   The workspace id
+     * @param string      $reportId  The report id
+     * @param string      datasetId  The new dataset for the rebound report. If the dataset resides in a different workspace than the report, a shared dataset will be created in the report's workspace
+     *
+     * @return \Tngnt\PBI\Response
+     */
+    public function rebindInGroup(string $groupId, string $reportId, string $datasetId)
+    {
+        $url = sprintf(self::GROUP_REPORT_REBIND_URL, $groupId, $reportId);
+
+        $body = [
+            'datasetId' => $datasetId,
         ];
 
         $response = $this->client->request(Client::METHOD_POST, $url, $body);
